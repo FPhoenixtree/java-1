2b60930a29e7234424722c4d1ff6c7b4954232de
FPhoenixtree <309890974@qq.com>
2021-01-08 10:26:34 +0800

    提交

--- src/project1/ShowCurrentTime.java
+++ src/project1/ShowCurrentTime.java
@@ -1,10 +1,13 @@
 package project1;
-
 public class ShowCurrentTime {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		long totalMilliseconds =System.currentTimeMillis();
+        long totalSecond = totalMilliseconds / 1000;
+        long currentSecond = totalSecond % 60;
+        long totalMinutes = totalSecond / 60;
+        long currentMinute = totalMinutes % 60;
+        long totalHours = totalMinutes / 60;
+        long currentHour = totalHours %24;
+        System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond + "GMT");
 	}
-
 }
