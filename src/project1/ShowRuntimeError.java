092911898faee50116bfca8ef587a6c881babcba
FPhoenixtree <309890974@qq.com>
2021-01-08 08:52:53 +0800

    提交

---
+++ src/project1/ShowRuntimeError.java
@@ -0,0 +1,6 @@
+package project1;
+public class ShowRuntimeError {
+	public static void main(String[] args) {
+		System.out.println(1 / 0);
+	}
+}
---
+++ src/project1/Welcome.java
@@ -0,0 +1,6 @@
+package project1;
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!");
+	}
+}
